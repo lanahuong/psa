dbb8e9486b6bb89337bc61c2018817902eab1752
Michel MIGDAL <michel.migdal@ensiie.fr>
2021-03-31 09:31:13 +0200

    Vectorised solving of the ODE

--- src/solver_ctcs.cpp
+++ src/solver_ctcs.cpp
@@ -1,46 +1,36 @@
 #include "solver_ctcs.h"
 
-void solver_ctcs::step() {
-    const arma::cx_mat &phi_prev = this->internal_state;
-
-    const int x_size(phi_prev.n_cols);
-    const int y_size(phi_prev.n_rows);
-
-    arma::cx_mat g_mat(phi_prev);
-    arma::cx_mat g_mat_prev;
-
-    do {
-        g_mat_prev = g_mat;
-        for (int x = 0; x < x_size; x++) {
-            for (int y = 0; y < y_size; y++) {
-                cross_cx t, g;
-                t.x_m_dx = x - 1 < 0 ? 0 : phi_prev.at(y, x - 1);
-                t.x_p_dx = x + 1 < x_size ? phi_prev.at(y, x + 1) : 0;
-                t.y_m_dy = y - 1 < 0 ? 0 : phi_prev.at(y - 1, x);
-                t.y_p_dy = y + 1 < y_size ? phi_prev.at(y + 1, x) : 0;
-
-                g.x_m_dx = x - 1 < 0 ? 0 : g_mat_prev.at(y, x - 1);
-                g.x_p_dx = x + 1 < x_size ? g_mat_prev.at(y, x + 1) : 0;
-                g.y_m_dy = y - 1 < 0 ? 0 : g_mat_prev.at(y - 1, x);
-                g.y_p_dy = y + 1 < y_size ? g_mat_prev.at(y + 1, x) : 0;
-
-                g_mat.at(x, y) = solver_ctcs::F(mat_pot.at(x, y), phi_prev.at(x, y), t, g);
-            }
-        }
-    } while (arma::norm(g_mat_prev - g_mat, "inf") > epsilon);
-
-    this->internal_state = g_mat;
-}
-
-cx solver_ctcs::F(double pot, cx psi, const cross_cx &t, const cross_cx &t_p_dt) const {
-    cx h2m = reduced_planck * reduced_planck / neutron_mass;
-    cx fact = cx(0, 2 * reduced_planck / dt) - pot - h2m * (1 / (dx * dx) + 1 / (dy * dy));
-    cx res = cx(0, 0);
-    res -= h2m / (2 * dx * dx) * (t.x_m_dx + t.x_p_dx + t_p_dt.x_m_dx + t_p_dt.x_p_dx);
-    res -= h2m / (2 * dy * dy) * (t.y_m_dy + t.y_p_dy + t_p_dt.y_m_dy + t_p_dt.y_p_dy);
-    res += (pot + cx(0, 2 * reduced_planck / dt) + h2m * (1 / (dx * dx) + 1 / (dy * dy))) * psi;
-    return res / fact;
-}
-
 solver_ctcs::solver_ctcs(const arma::cx_mat &phi_0, const arma::mat &pot, double dx, double dy, double dt)
         : solver(phi_0, pot, dx, dy, dt) {}
+
+void solver_ctcs::step() {
+    const auto pot(add_zero_border(mat_pot));
+    const auto f_t(add_zero_border(this->internal_state));
+    arma::cx_mat g_np1 = f_t, g_n;
+
+    const arma::cx_mat f_t_pm_dx = shift_mat(f_t, 0, -1) + shift_mat(f_t, 0, 1);
+    const arma::cx_mat f_t_pm_dy = shift_mat(f_t, -1, 0) + shift_mat(f_t, 1, 0);
+
+    const cx h2m = reduced_planck * reduced_planck / neutron_mass;
+    const arma::cx_mat fact = cx(0, 2 * reduced_planck / dt) - h2m * (1 / (dx * dx) + 1 / (dy * dy)) - pot;
+    const arma::cx_mat fact2 = (pot + cx(0, 2 * reduced_planck / dt) + h2m * (1 / (dx * dx) + 1 / (dy * dy)));
+
+    do {
+        g_n = g_np1;
+
+        arma::cx_mat x_m_dx = shift_mat(g_n, 0, -1);
+        arma::cx_mat x_p_dx = shift_mat(g_n, 0, 1);
+        arma::cx_mat y_m_dy = shift_mat(g_n, -1, 0);
+        arma::cx_mat y_p_dy = shift_mat(g_n, 1, 0);
+
+        g_np1 = [&]() {
+            arma::cx_mat res(-h2m / (2 * dx * dx) * (f_t_pm_dx + x_m_dx + x_p_dx));
+            res -= h2m / (2 * dy * dy) * (f_t_pm_dy + y_m_dy + y_p_dy);
+            res += fact2 % f_t;
+            return res / fact;
+        }();
+
+    } while (arma::norm(g_n - g_np1, "inf") > epsilon);
+
+    this->internal_state = shed_zero_border(g_np1);
+}
--- src/solver_ctcs.h
+++ src/solver_ctcs.h
@@ -3,18 +3,12 @@
 
 #include "solver.h"
 
-struct cross_cx {
-    cx x_m_dx, x_p_dx, y_m_dy, y_p_dy;
-};
-
 class solver_ctcs : public solver {
-private:
-    cx F(double pot, cx psi, const cross_cx &t, const cross_cx &t_p_dt) const;
-
 public:
     solver_ctcs(const arma::cx_mat &phi_0, const arma::mat &pot, double dx, double dy, double dt);
 
     void step() override;
+
 };
 
 #endif //PROJET_PSA_SOLVER_CTCS_H
