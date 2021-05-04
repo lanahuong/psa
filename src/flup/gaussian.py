a73a775c244614cb855be1b6475e09ccb61b704f
Mathis Boiteau <mathis.boiteau@gmail.com>
2021-05-04 15:45:42 +0200

    Adding the complex part of the gaussian, linked to it's speed in the .json

--- src/flup/gaussian.py
+++ src/flup/gaussian.py
@@ -16,6 +16,7 @@ def normalize(m, dimensions, shape):
 def createMapGaussian(wave, dimensions, shape):
     coordX, coordY = wave["Coord"]
     width = wave["Width"]
+    kx,ky = wave["Speed"]
     mapSizeX, mapSizeY = dimensions
     nbMeshX, nbMeshY = shape
     x, y = np.meshgrid(
@@ -23,7 +24,7 @@ def createMapGaussian(wave, dimensions, shape):
         np.linspace(-mapSizeY, mapSizeY, nbMeshY),
     )
     g = np.exp(-(((x - coordX) ** 2 + (y - coordY) ** 2) / (width ** 2)))
-    return normalize(g, dimensions, shape)
+    return normalize(g, dimensions, shape)*np.exp(1j * (kx * x + ky * y))
 
 
 # Obsolete
@@ -36,7 +37,7 @@ def afficheInit(field, init):
     hf = plt.figure()
     ha = hf.add_subplot(111, projection="3d")
 
-    X, Y = np.meshgrid(x, y)  # `plot_surface` expects `x` and `y` data to be 2D
+    X, Y = np.meshgrid(x, y)  
     ha.plot_surface(X, Y, init)
 
     plt.show()
