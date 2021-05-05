ae26e4b4a1ac4dd66bb31ac0677659dbb10e0b6b
Michel MIGDAL <michel.migdal@ensiie.fr>
2021-05-05 17:07:38 +0200

    Tests with mazes and various particles amount

--- bindings/test_labyrinthe.py
+++ bindings/test_labyrinthe.py
@@ -53,12 +53,27 @@ def normalize(m, size):
     return m / (norm * ds)
 
 
-def createMapGaussian(size):
-    coordX = 20
-    coordY = 20
-    width = 2 * size/5
-    kx = -10000 * size/5
+def createMapGaussian_a(size):
+    coordX = 40
+    coordY = 40
+    width =  size/5
+    kx = 10000 * size/5
     ky = 10000 * size/5
+    mapSizeX, mapSizeY = size,size
+    nbMeshX, nbMeshY = size,size
+    x, y = np.meshgrid(
+        np.linspace(-mapSizeX, mapSizeX, nbMeshX),
+        np.linspace(-mapSizeY, mapSizeY, nbMeshY),
+    )
+    g = np.exp(-(((x - coordX) ** 2 + (y - coordY) ** 2) / (width ** 2)))
+    return normalize(g,size)*np.exp(1j * (kx * x + ky * y))
+
+def createMapGaussian_b(size):
+    coordX = -40
+    coordY = -40
+    width =  size/5
+    kx = 10000 * size/5
+    ky = -10000 * size/5
     mapSizeX, mapSizeY = size,size
     nbMeshX, nbMeshY = size,size
     x, y = np.meshgrid(
@@ -71,8 +86,13 @@ def createMapGaussian(size):
 
 
 def run_anim(dx, dy, dt, temporal_supersampling, size, frames_count, out_name):
-    mat = createMapGaussian(size)
-    solver = flus.SchemeCTCS( np.asfortranarray(mat), np.asfortranarray(np.zeros((size, size))),dx,dy, dt/temporal_supersampling  )
+    mat = createMapGaussian_a(size) + createMapGaussian_b(size)
+
+    #potential = np.asfortranarray(np.zeros((size, size)))
+    potential = np.asfortranarray(load_image("maze_large.jpg"))
+    potential = np.asfortranarray(load_image("maze2.png"))
+
+    solver = flus.SchemeCTCS( np.asfortranarray(mat), potential,dx,dy, dt/temporal_supersampling  )
     mat = np.absolute(mat)
     fig = plt.figure()
     plot = plt.matshow(mat, fignum=0)
@@ -89,7 +109,6 @@ def run_anim(dx, dy, dt, temporal_supersampling, size, frames_count, out_name):
     anim.save(out_name, writer='imagemagick', fps=60)
     #plt.show()
 
-#run_anim(1e-4,1e-4,1e-3, 10, 50, 100, "anim_cassée3.gif")
-#run_anim(0.005,0.005, 1e15, 100, 256, 100, "anim_passable.gif")
-run_anim(0.125,0.125, 2e18, 10, 128, 180, "anim_passable.gif")
-#run_anim(0.125,0.125, 1e18, 10, 256, 180, "anim_passable.gif")
+#run_anim(0.0125,0.0125, 1e16, 10, 800, 100, "anim_passable.gif")
+run_anim(0.125,0.125, 1e17, 10, 128, 4, "anim_passable.gif")
+
--- src/flus/SchemeCTCS.cpp
+++ src/flus/SchemeCTCS.cpp
@@ -20,8 +20,8 @@ void SchemeCTCS::step() {
     const auto n1 = m_e_scale_ * step_scale_ * step_scale_ / (h_bar_scale_ * h_bar_scale_);
     const auto n2 = m_e_scale_ * h_bar_scale_ / step_scale_;
 
-    const arma::cx_mat f_t_pm_dx = phit_.submat(2, 1, size(phitdt_)) + phit_.submat(0, 1, size(phitdt_));
-    const arma::cx_mat f_t_pm_dy = phit_.submat(1, 2, size(phitdt_)) + phit_.submat(1, 0, size(phitdt_));
+    const arma::cx_mat f_t_pm_dx = phit_(2, 1, size(phitdt_)) + phit_(0, 1, size(phitdt_));
+    const arma::cx_mat f_t_pm_dy = phit_(1, 2, size(phitdt_)) + phit_(1, 0, size(phitdt_));
 
     const auto pot_sub = pot.submat(1, 1, size(phitdt_));
     const cx h2m = h_bar_ * h_bar_ / m_e_;
@@ -30,13 +30,13 @@ void SchemeCTCS::step() {
 
     do {
         g_n = g_np1;
-        arma::cx_mat gn_pm_dx = g_n.submat(2, 1, size(phitdt_)) + g_n.submat(0, 1, size(phitdt_));
-        arma::cx_mat gn_pm_dy = g_n.submat(1, 2, size(phitdt_)) + g_n.submat(1, 0, size(phitdt_));
-        arma::cx_mat res = -(h2m / (2 * dx_ * dx_)) * (f_t_pm_dx + gn_pm_dx) - (h2m / (2 * dy_ * dy_)) * (f_t_pm_dy + gn_pm_dy) + fact2 % phit_.submat(1, 1, size(phitdt_));
-        g_np1.submat(1, 1, size(phitdt_)) = res % fact;
-    } while (arma::norm(g_n.submat(1, 1, size(phitdt_)) - g_np1.submat(1, 1, size(phitdt_)), "inf") > epsilon);
+        arma::cx_mat gn_pm_dx = g_n(2, 1, size(phitdt_)) + g_n(0, 1, size(phitdt_));
+        arma::cx_mat gn_pm_dy = g_n(1, 2, size(phitdt_)) + g_n(1, 0, size(phitdt_));
+        arma::cx_mat res = -(h2m / (2 * dx_ * dx_)) * (f_t_pm_dx + gn_pm_dx) - (h2m / (2 * dy_ * dy_)) * (f_t_pm_dy + gn_pm_dy) + fact2 % phit_(1, 1, size(phitdt_));
+        g_np1(1, 1, size(phitdt_)) = res % fact;
+    } while (arma::norm(g_n(1, 1, size(phitdt_)) - g_np1(1, 1, size(phitdt_)), "inf") > epsilon);
 
     phit_ = g_np1;
-    phitdt_ = g_np1.submat(1, 1, size(phitdt_));
+    phitdt_ = g_np1(1, 1, size(phitdt_));
     //phitdt_ = phit_.submat(1, 1, size(phitdt_));
 }
