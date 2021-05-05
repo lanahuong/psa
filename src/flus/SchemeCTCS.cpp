8e246aab6685c676697f9d45a10129b46a789e8c
Michel MIGDAL <michel.migdal@ensiie.fr>
2021-05-05 12:46:54 +0200

    Update to ctcs

--- src/flus/SchemeCTCS.cpp
+++ src/flus/SchemeCTCS.cpp
@@ -12,17 +12,21 @@ SchemeCTCS::SchemeCTCS(const arma::cx_mat &phi_0, const arma::mat &pot,
         : Solver(phi_0, pot, dx, dy, dt) {}
 
 void SchemeCTCS::step() {
-   // GILReleaser releaser;
+    // GILReleaser releaser;
     const auto pot = mat_pot_;
     arma::cx_mat g_np1 = phit_, g_n;
+
+
+    const auto n1 = m_e_scale_ * step_scale_ * step_scale_ / (h_bar_scale_ * h_bar_scale_);
+    const auto n2 = m_e_scale_ * h_bar_scale_ / step_scale_;
 
     const arma::cx_mat f_t_pm_dx = phit_.submat(2, 1, size(phitdt_)) + phit_.submat(0, 1, size(phitdt_));
     const arma::cx_mat f_t_pm_dy = phit_.submat(1, 2, size(phitdt_)) + phit_.submat(1, 0, size(phitdt_));
 
     const auto pot_sub = pot.submat(1, 1, size(phitdt_));
     const cx h2m = h_bar_ * h_bar_ / m_e_;
-    const arma::cx_mat fact = 1 / (cx(0, 2 * h_bar_ / dt_) - h2m * (1 / (dx_ * dx_) + 1 / (dy_ * dy_)) - pot_sub);
-    const arma::cx_mat fact2 = pot_sub + cx(0, 2 * h_bar_ / dt_) + h2m * (1 / (dx_ * dx_) + 1 / (dy_ * dy_));
+    const arma::cx_mat fact = 1 / (n2 * cx(0, 2 * h_bar_ / dt_) - h2m * (1 / (dx_ * dx_) + 1 / (dy_ * dy_)) - n1 * pot_sub);
+    const arma::cx_mat fact2 = n1 * pot_sub + n2 * cx(0, 2 * h_bar_ / dt_) + h2m * (1 / (dx_ * dx_) + 1 / (dy_ * dy_));
 
     do {
         g_n = g_np1;
