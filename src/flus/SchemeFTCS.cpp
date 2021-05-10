ca0939ac026d9ed11c853a11b0ea2c96df7a9afc
Michel MIGDAL <michel.migdal@ensiie.fr>
2021-05-10 19:32:52 +0200

    Finally FTCS is fixed ! Works well with a supersampling of 1000

--- src/flus/SchemeFTCS.cpp
+++ src/flus/SchemeFTCS.cpp
@@ -8,17 +8,16 @@ void SchemeFTCS::step() {
     /** i^2 = -1 */
     cx i(0, 1);
     // Compute phitdt
-    arma::cx_mat xpart = -(h_bar_ * h_bar_) / (2 * m_e_ * dx_ * dx_) *
+    auto s = arma::size(phit_.n_rows - 4, phit_.n_cols - 4);
+    arma::cx_mat xpart = -h_bar_ * h_bar_ / (2 * m_e_ * dx_ * dx_) *
                          (phit_.submat(2, 1, size(phitdt_)) + phit_.submat(0, 1, size(phitdt_)));
-
     arma::cx_mat ypart = -h_bar_ * h_bar_ / (2 * m_e_ * dy_ * dy_) *
                          (phit_.submat(1, 2, size(phitdt_)) + phit_.submat(1, 0, size(phitdt_)));
+    arma::cx_mat centerpart = (mat_pot_ % phit_) + ((i * h_bar_ / dt_) + (h_bar_ * h_bar_ / (m_e_ * dx_ * dx_)) + (h_bar_ * h_bar_ / (m_e_ * dy_ * dy_))) * phit_;
+    arma::cx_mat tmp = centerpart.submat(1, 1, size(phitdt_));
+    tmp(1, 1, s) += xpart(1, 1, s) + ypart(1, 1, s);
 
-    cx mat_pot_factor = m_e_scale_ * step_scale_ * step_scale_ / (h_bar_scale_ * h_bar_scale_);
-    cx prefactor = m_e_scale_ * step_scale_ / h_bar_scale_;
-    arma::cx_mat centerpart = (mat_pot_ * mat_pot_factor % phit_) + ((prefactor * i * h_bar_ / dt_) + (h_bar_ * h_bar_ / (m_e_ * dx_ * dx_)) + (h_bar_ * h_bar_ / (m_e_ * dy_ * dy_))) * phit_;
-    arma::cx_mat tmp = xpart + ypart + centerpart.submat(1, 1, size(phitdt_));
-    phitdt_ = tmp * (dt_ / (i * h_bar_ * prefactor));
+    phitdt_ = tmp * (dt_ / (i * h_bar_));
     // Copy phitdt to phit for the next iteration with correct 0 padding
     phit_.submat(1, 1, size(phitdt_)) = phitdt_;
 }
