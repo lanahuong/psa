0fc2262c5847f97b5eccb00a19f42d8d13f7fc10
Michel Migdal <contact@mgdal.fr>
2021-05-11 16:02:16 +0200

    Silencing swig warnings

--- bindings/setup.py
+++ bindings/setup.py
@@ -7,8 +7,9 @@ module1 = Extension(
     "_flus",
     include_dirs=["./include/armanpy/", numpy.get_include(), get_python_inc()],
     libraries=["m", "z", "armadillo"],
-    extra_compile_args=["-fopenmp", "-Ofast", "-march=native", "-mtune=native"],
-    extra_link_args=["-fopenmp"],
+    extra_compile_args=["-Ofast", "-march=native", "-mtune=native",
+                        "-Wno-delete-non-virtual-dtor", "-Wno-missing-field-initializers", "-Wno-deprecated-declarations"], #silencing swig warnings
+    extra_link_args=["-larmadillo"],
     sources=[
         "flus.i",
         "../src/flus/SchemeFTCS.cpp",
@@ -16,7 +17,8 @@ module1 = Extension(
         "../src/flus/SchemeCTCS.cpp",
         "../src/flus/Solver.cpp",
     ],
-    swig_opts=["-c++", "-Wall", "-I.", "-I./include/armanpy/", "-threads"],
+    swig_opts=["-c++", "-Wall", "-Wextra", "-I.",
+               "-I./include/armanpy/", "-threads"],
 )
 
 setup(
