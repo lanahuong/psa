1e441d59bc2a83db36ed860c427d1f9542e0b561
Michel Migdal <contact@mgdal.fr>
2021-05-11 16:14:56 +0200

    Added fopenmp

--- bindings/setup.py
+++ bindings/setup.py
@@ -7,7 +7,7 @@ module1 = Extension(
     "_flus",
     include_dirs=["./include/armanpy/", numpy.get_include(), get_python_inc()],
     libraries=["m", "z", "armadillo"],
-    extra_compile_args=["-Ofast", "-march=native", "-mtune=native", "-fopenmp", "-larmadillo",
+    extra_compile_args=["-Ofast", "-march=native", "-mtune=native", "-fopenmp",
                         "-Wno-delete-non-virtual-dtor", "-Wno-missing-field-initializers", "-Wno-deprecated-declarations"], #silencing swig warnings
     extra_link_args=["-fopenmp", "-larmadillo"], #for ARMADILLO
     sources=[
